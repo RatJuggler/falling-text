60ade36377abb5c33f013ae44e11d03add6c391e
John Chase <ratteal@gmail.com>
2020-02-28 17:38:50 +0000

    Refactor tidy.

--- js/falling-text.js
+++ js/falling-text.js
@@ -1,23 +1,31 @@
 // Look up the canvas element and get the context.
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const ctx = document.getElementById("canvas").getContext("2d");
 
-// The canvas and font sizes govern the number of rows and columns available for text.
-var font_size = 16;
-var render_font = font_size + "px arial";
-var rows, columns;
+// Track font render details and text to use.
+let renderFont, textRepo;
 
 // An array to hold the number of falling texts we want to show.
-var falling_text = new Array(9);
+const falling_text = new Array(9);
 
 // Keep track of the animation interval.
-var intervalId;
+let intervalId;
 
-// A repository of the text availble for falling.
+// Details of
+class RenderFont {
+	constructor() {
+		// The canvas and font sizes govern the number of rows and columns available for text.
+		this.font_size = 16;
+		this.render_font = this.font_size + "px arial";
+		this.rows = ctx.canvas.height / this.font_size;
+		this.columns = ctx.canvas.width / this.font_size;
+	}
+}
+
+// A repository of the text available for falling.
 class TextRepository {
 	//repository;  // Contains all the text we could use.
 	//n;           // Index of the next text to use.
-	// Define the inital text to show while the file loads.
+	// Define the initial text to show while the file loads.
 	constructor() {
 		this.repository = ["TYRANNOSAURUS", "TRICERATOPS", "VELOCIRAPTOR", "STEGOSAURUS", "SPINOSAURUS", "ARCHAEOPTERYX", "BRACHIOSAURUS", "ALLOSAURUS", "APATOSAURUS", "DILOPHOSAURUS"];
 		this.n = 0;
@@ -29,7 +37,7 @@ class TextRepository {
 			.then(text => this.repository = text.split('\n').map(s => s.toUpperCase()));
 	}
 	getNextText() {
-		if (this.n == this.repository.length) {
+		if (this.n === this.repository.length) {
 			this.n = 0;
 		}
 		return this.repository[this.n++];
@@ -61,20 +69,20 @@ class FallingText {
 	}
 }
 
-function intialise() {
+function initialise() {
 	// Stop any existing animation.
 	clearInterval(intervalId);
 	// Make the canvas match the window size.
-	canvas.height = window.innerHeight;
-	canvas.width = window.innerWidth;
-	// Work out how many text rows and columns this gives us.
-	rows = canvas.height / font_size;
-	columns = canvas.width / font_size;
-	// Populate initial display.
+	ctx.canvas.height = window.innerHeight;
+	ctx.canvas.width = window.innerWidth;
+	// Initialise an instance of the font rendering details.
+	renderFont = new RenderFont();
+	// Initialise an instance of the text repository.
 	textRepo = new TextRepository();
+	// Populate initial display.
 	textRepo.populateFromFile("https://raw.githubusercontent.com/junosuarez/dinosaurs/master/dinosaurs.csv");
 	for(let i = 0; i < falling_text.length; i++) {
-		falling_text[i] = new FallingText(textRepo.getNextText(), rows, columns);
+		falling_text[i] = new FallingText(textRepo.getNextText(), renderFont.rows, renderFont.columns);
 	}
 	// Run the animation.
 	intervalId = setInterval(render, 100);
@@ -84,22 +92,22 @@ function intialise() {
 function render() {
 	// Use a black background for the canvas with translucency for the trail effect.
 	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 	// Set green text in the required font.
 	ctx.fillStyle = "#00FF00";
-	ctx.font = render_font;
+	ctx.font = renderFont.render_font;
 	// Loop through the texts to display...
 	for(let i = 0; i < falling_text.length; i++) {
 		let display_text = falling_text[i];
 		// Display the text.
-		display_text.render(font_size);
+		display_text.render(renderFont.font_size);
 		// Move it down the screen.
-		display_text.moveDown(rows, columns);
+		display_text.moveDown(renderFont.rows, renderFont.columns);
 	}
 }
 
 // Be ready to restart the animation if the window size changes.
-window.addEventListener("resize", intialise);
+window.addEventListener("resize", initialise);
 
 // Kick off the animation.
-intialise();
+initialise();
